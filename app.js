67153a00e538aae601aa8ca79336a8431ca40be0
leandrosegovia22 <leandrosegoviaroman@gmail.com>
2021-08-10 12:16:56 -0300

    Primer Commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require ('express'); 
+const app = express(); 
+const port = 3030; 
+const path = require('path'); 
+const views = path.join(__dirname, 'views'); 
+
+app.use(express.static('public')); 
+
+// Rutas
+app.get('/', (req, res)=> res.sendFile(path.join(views,'home.html')) );
+app.get('/register', (req, res)=> res.sendFile(path.join(views,'register.html')) ); 
+app.get('/login', (req, res)=> res.sendFile(path.join(views,'login.html')) );
+
+app.listen(port, ()=> console.log('Servidor corriendo en el puerto ' + port)); 
